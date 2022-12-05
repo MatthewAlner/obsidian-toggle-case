76073f61815080b1816e8920ef385106300676ea
Matthew Alner <matthewalner@googlemail.com>
2022-12-05 21:15:05 +0000

    Remove some unneeded sample code

--- build-scripts/deploy-local.sh
+++ build-scripts/deploy-local.sh
@@ -3,6 +3,11 @@
 OBSIDIAN_PLUGIN_PATH="$HOME/obsidian-vaults/Sandbox/.obsidian/plugins"
 OBSIDIAN_PLUGIN_NAME="toggle-case"
 
+echo "running local deploy:"
+echo "PATH: ${OBSIDIAN_PLUGIN_PATH:?}/${OBSIDIAN_PLUGIN_NAME:?}"
+
 rm -rf "${OBSIDIAN_PLUGIN_PATH:?}/${OBSIDIAN_PLUGIN_NAME:?}"
 mkdir -p "${OBSIDIAN_PLUGIN_PATH:?}/${OBSIDIAN_PLUGIN_NAME:?}"
 cp main.js styles.css manifest.json "${OBSIDIAN_PLUGIN_PATH:?}/${OBSIDIAN_PLUGIN_NAME:?}"
+
+printf "$(tput setaf 2)✓ Success: $(tput sgr0)%s.\n" "Restart Obsidian"
--- main.ts
+++ main.ts
@@ -16,24 +16,12 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'toggle-case',
+			name: 'Toggle Case',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new Notice('Do thing!')
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -43,25 +31,6 @@ export default class MyPlugin extends Plugin {
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
 			}
 		});
 
@@ -88,22 +57,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
